124ae8daebd86d6cc9257bc8f934515c2d989043
Pedro Brito <156836900+PedroBrito0519@users.noreply.github.com>
2024-10-28 17:51:34 -0300

    Update avaliacao.js

--- js/avaliacao.js
+++ js/avaliacao.js
@@ -56,7 +56,7 @@ function renderFeedback() {
         li.innerHTML = `
             <span>${feedback}</span>
             <div>
-                <button class="form_button" onclick="openEditModal(${index})"><a href="index.html">Editar</a></button>
+                <button class="form_button" onclick="openEditModal(${index})">Editar</button>
             </div>
         `;
         feedbackList.appendChild(li);
